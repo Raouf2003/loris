13947ae10a43add9dcd3d77029764abef95a477d
Raouf Loukriz <raoufloukriz8@gmail.com>
2026-06-22 19:26:36 +0200

    fix: explicit db name instead of get_default_database

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from config import settings
 
 client = AsyncIOMotorClient(settings.mongodb_uri)
-db = client.get_default_database()
+db = client.kiosk
 
 
 async def init_db():
